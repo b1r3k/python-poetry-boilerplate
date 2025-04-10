e86e0dc2d2ec843bf65b4d2844b3cfbe98fb60ed
Lukasz Jachym <lukasz.jachym@gmail.com>
2025-04-10 18:59:04 +0200

    adds: root logger with config from settings

--- app_name/main.py
+++ app_name/main.py
@@ -1,13 +1,14 @@
 import argparse
 
 from .config import settings
+from .logging import root_logger as logger
 
 
 def cli():
     parser = argparse.ArgumentParser()
     parser.add_argument("name", help="your name")
     args = parser.parse_args()
-    print(f"Hello, {args.name}! My name is {settings.APP_NAME}")
+    logger.info(f"Hello, {args.name}! My name is {settings.APP_NAME}")
 
 
 if __name__ == "__main__":
